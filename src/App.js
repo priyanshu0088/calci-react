e5e09b7e11bd9262eea0e13d265e45da660a9d76
priyanshu0088 <112187158+priyanshu0088@users.noreply.github.com>
2023-01-04 22:11:42 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [text,settext] =useState("");
+
+  const handleonchange = (e) =>{
+    settext(text.concat(e.target.name));
+  }
+
+  const Allempty = () =>{
+    settext("");       
+  } 
+
+  const backspace =()=>{
+    settext(text.slice(0,-1));
+  }
+
+  const result = () =>{
+    try{
+      settext(eval(text).toString());
+    } catch(err){
+      settext("Error")
+    }
+  } 
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        
+      <div className="center"><input className='input-no' value={text} type="text" />
+      <div className="calc-btn">
+        <button name='1'  onClick={handleonchange} className='butn'>1</button>
+        <button  name='2' onClick={handleonchange} className='butn'>2</button>
+        <button  name='3' onClick={handleonchange} className='butn'>3</button> 
+        <button   onClick={backspace} className='special' id='backspace'>X</button>
+        <button  name='4' onClick={handleonchange} className='butn'>4</button>
+        <button  name='5' onClick={handleonchange} className='butn'>5</button>
+        <button  name='6' onClick={handleonchange} className='butn'>6</button>
+       
+        <button name='/' onClick={handleonchange}  className='special'>/</button>
+        <button name='7' onClick={handleonchange} className='butn'>7</button>
+        <button  name='8' onClick={handleonchange} className='butn'>8</button>
+        <button name='9' onClick={handleonchange} className='butn'>9</button>                          
+        <button  name='*' onClick={handleonchange} className='special'>*</button>
+        <button  name='0' onClick={handleonchange} className='butn'>0</button>
+      
+        <button name='+'  onClick={handleonchange} className='butn' >+</button>
+        <button  name='-'onClick={handleonchange} className='butn' >-</button>
+        
+        <button  name='.' onClick={handleonchange}className='special' id='dot' >.</button>
+        <button  onClick={Allempty} className='special' id='clear-all'>clear</button>
+        <button  onClick={result} className='butn' id='result'>=</button>
+        
+</div>
+        </div>
     </div>
   );
 }
